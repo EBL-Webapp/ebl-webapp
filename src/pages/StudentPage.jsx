53e3537e35b198c74b4f5745de49077fd658d72b
devRaxx <rpcastaneros@up.edu.ph>
2025-04-16 00:53:48 +0800

    blank

--- src/pages/StudentPage.jsx
+++ src/pages/StudentPage.jsx
@@ -1,5 +1,9 @@
 function StudentPage() {
-    return <h1 className="text-3xl font-bold text-center mt-10">Welcome to the Student Page</h1>;
-  }
-  
-  export default StudentPage;
+  return (
+    <h1 className="text-3xl font-bold text-center mt-10">
+      Welcome to the Student Page
+    </h1>
+  );
+}
+
+export default StudentPage;
