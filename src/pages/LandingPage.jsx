e9cb944e947ca04a33e6e728dccdb051cdf114ac
Cedric Siosana <cedricpsiosana@gmail.com>
2026-06-21 11:06:35 +0800

    fix: Removed transients

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -128,9 +128,9 @@ const LandingPage = () => {
             </div>
 
             {/* Call to Action Button */}
-            <button onClick={() => navigate('/transient')} className="bg-black hover:bg-gray-900 text-white font-semibold py-3 px-6 sm:px-8 rounded-lg transition duration-300 text-lg sm:text-lg shadow-md">
+            {/* <button onClick={() => navigate('/transient')} className="bg-black hover:bg-gray-900 text-white font-semibold py-3 px-6 sm:px-8 rounded-lg transition duration-300 text-lg sm:text-lg shadow-md">
               Just Visiting? Check the Transient Rates!
-            </button>
+            </button> */}
           </section>
         </div>
       </main>
